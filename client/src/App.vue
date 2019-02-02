3454b83d6e24dbda9c99c76e87c5f1d7a41ea1cf
Nightwatch <nightwatch@MacBook-Pro-polzovatel.local>
2019-02-02 15:06:00 +0300

    init

---
+++ client/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/create" class="nav-link">Create Post</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/posts" class="nav-link">Posts</router-link>
+        </li>
+      </ul>
+    </nav><br />
+
+    <transition name="fade">
+      <router-view>
+
+      </router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+import HomeComponent from './components/HomeComponent.vue'
+import CreateComponent from './components/CreateComponent.vue'
+import IndexComponent from './components/IndexComponent.vue';
+import EditComponent from './components/EditComponent.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HomeComponent,
+    CreateComponent,
+    IndexComponent,
+    EditComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+</style>
