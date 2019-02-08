b0d5028b4da06302861e20c27be14385ed4cdffc
Nightwatch <nightwatch@MacBook-Pro-polzovatel.local>
2019-02-08 23:13:21 +0300

    display DB data, update, delete

--- client/src/components/EditComponent.vue
+++ client/src/components/EditComponent.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="row justify-content-center">
-      <div class="col-md-8">
-          <div class="card card-default">
-              <div class="card-header">Edit Component</div>
-
-              <div class="card-body">
-                  I'm an Edit component.
-              </div>
+  <div>
+    <h1>Edit Client</h1>
+    <form @submit.prevent="updateClient">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Name:</label>
+              <input type="text" class="form-control" v-model="client.name">
+            </div>
           </div>
-      </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Email</label>
+              <input type="text" class="form-control" v-model="client.email">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Phone</label>
+              <input type="text" class="form-control" v-model="client.phone">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Providers</label>
+              <textarea class="form-control" v-model="client.providers" rows="5"></textarea>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div class="form-group">
+          <button class="btn btn-primary">Update</button>
+        </div>
+    </form>
   </div>
 </template>
 
 <script>
   export default {
-  }
+    data () {
+      return {
+        client: {}
+      };
+    },
+    created() {
+      let uri = `http://localhost:4000/clients/edit/${this.$route.params.id}`;
+      this.axios.get(uri).then(response => {
+        this.client = response.data; // change to response.data.client
+      });
+    },
+    methods: {
+      updateClient() {
+        let uri = `http://localhost:4000/clients/update/${this.$route.params.id}`;
+        this.axios.post(uri, this.client).then(() => {
+          this.$router.push({name: 'clients'});
+        });
+      }
+    }
+  };
 </script>
--- client/src/components/IndexComponent.vue
+++ client/src/components/IndexComponent.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="row justify-content-center">
-      <div class="col-md-8">
-          <div class="card card-default">
-              <div class="card-header">Index Component</div>
-
-              <div class="card-body">
-                  I'm an Index component.
-              </div>
+  <div>
+      <h1>Clients</h1>
+        <div class="row">
+          <div class="col-md-10"></div>
+          <div class="col-md-2">
+            <router-link :to="{ name: 'create' }" class="btn btn-primary">Create Client</router-link>
           </div>
-      </div>
+        </div><br />
+
+        <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Providers</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr v-for="client in clients" :key="client._id">
+                  <td>{{ client.name }}</td>
+                  <td>{{ client.email }}</td>
+                  <td>{{ client.phone }}</td>
+                  <td>{{ client.providers }}</td>
+                  <td><router-link :to="{name: 'edit', params: { id: client._id }}" class="btn btn-primary">Edit</router-link></td>
+                  <td><button class="btn btn-danger" @click.prevent="deleteClient(client._id)">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
   </div>
 </template>
 
 <script>
   export default {
-  }
+    data() {
+      return {
+        clients: []
+      };
+    },
+    created() {
+      let uri = 'http://localhost:4000/clients';
+      this.axios.get(uri).then(response => {
+        this.clients = response.data;
+      });
+    },
+    methods: {
+      deleteClient(id) {
+        let uri = `http://localhost:4000/clients/delete/${id}`;
+        this.axios.delete(uri).then(response => {
+          this.clients.splice(this.clients.indexOf(id), 1);
+        });
+      }
+    }
+  };
 </script>
